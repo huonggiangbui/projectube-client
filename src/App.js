ab4ea995c6995010307d049e0b5b037524caf8d3
huonggiangbui <bui.huonggiang4ahht@gmail.com>
2020-04-12 02:42:49 +0700

    Use Apollo

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import React, { useEffect, useState } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
 
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+
 import Navbar from './components/Navbar';
 
 import firebase from 'firebase';
 import 'firebase/auth';
 
+const QUERY = gql`
+  {
+    orgs {
+      _id,
+      name
+    }
+  }
+`;
+
 export default function App() {
   const [ user, updateUser ] = useState(null);
+  const { loading, error, data } = useQuery(QUERY);
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
@@ -21,11 +34,20 @@ export default function App() {
     });
   }, []);
 
+  if (loading) return 'Loading...';
+  if (error) {
+    console.log(error);
+    return 'Error :(';
+  }
+
+  console.log(data.orgs);
+
   return (
     <ThemeProvider theme={theme}>
       <Navbar 
         user={user} 
       />
+      { data.orgs[0].name }
     </ThemeProvider>
   );
 }
